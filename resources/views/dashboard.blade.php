ce02abff5e6274ae8839fa30f307c8ac9430a2aa
Rafael Lacerda Busatta <lacerdabusatta@gmail.com>
2025-01-30 17:29:44 -0300

    table insertion

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,36 +1,201 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('SGPAD') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="row mx-0">
+        <!-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
-        </div>
-        <div class="navbar sticky-top bg-light flex-md-nowrap p-0 shadow" data-bs-theme="dark">
-            <ul class="navbar-nav flex-row d-md-none">
-                <li class="nav-item text-nowrap">
-                <button class="nav-link px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSearch" aria-controls="navbarSearch" aria-expanded="false" aria-label="Toggle search">
-                    <svg class="bi"><use xlink:href="#search"/></svg>
-                    oi
-                </button>
-                </li>
-                <li class="nav-item text-nowrap">
-                <button class="nav-link px-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                    <svg class="bi"><use xlink:href="#list"/></svg>
-                </button>
-                </li>
-            </ul>
+        </div> -->
+        <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+            <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
+                        <span>Cadastros</span>
+                        <a class="link-secondary" href="#" aria-label="Add a new report">
+                        </a>
+                    </h6>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Abertura de contas
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Avaiação de Projetos
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Renovação de contas
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Cadastrar Publicação
+                            </a>
+                        </li>
+                    </ul>
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
+                        <span>Consultas</span>
+                        <a class="link-secondary" href="#" aria-label="Add a new report">
+                        </a>
+                    </h6>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Projetos
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Usuário
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Publicações
+                            </a>
+                        </li>
+                    </ul>
 
-            <div id="navbarSearch" class="navbar-search w-100 collapse">
-                <input class="form-control w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
+                        <span>Relatórios</span>
+                        <a class="link-secondary" href="#" aria-label="Add a new report">
+                        </a>
+                    </h6>
+                    <ul class="nav flex-column mb-auto">
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Projlist
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Estatísticas
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Contas Expiradas
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Contas Desativadas
+                            </a>
+                        </li>
+                    </ul>
+
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
+                        <span>Outras Informações</span>
+                        <a class="link-secondary" href="#" aria-label="Add a new report">
+                        </a>
+                    </h6>
+                    <ul class="nav flex-column mb-auto">
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Instituições
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Orientador
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Pesquisar Lattes
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="#">
+                                Logs do Sistema
+                            </a>
+                        </li>
+                    </ul>
+
+                <hr class="my-3">
+
+                <ul class="nav flex-column mb-auto">
+                    <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                        Settings
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                        Sign out
+                    </a>
+                    </li>
+                </ul>
+                </div>
             </div>
+        </div>
+        <div class="col-md-9 col-lg-10 p-0">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <table class="table style-table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">Projeto</th>
+                                    <th scope="col">Data</th>
+                                    <th scope="col">Confirmação do Usuário</th>
+                                    <th scope="col">Reenviar e-mail</th>
+                                    <th scope="col">Confirmação do Orientador</th>
+                                    <th scope="col">Aprovação pelo CAP</th>
+                                    <th scope="col">Avaliar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="row-odd">
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                    <td>Otto</td>
+                                </tr>
+                                <tr class="row-even">
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                    <td>Thornton</td>
+                                </tr>
+                                <tr>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                    <td>the Bird</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>  
         </div>
     </div>
 </x-app-layout>
